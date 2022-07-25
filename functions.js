ad2b24009a124ed5ade0f4f3ebb521bdecf4e05b
Manul Wickramanayaka <mwickraman@wiley.com>
2022-07-25 23:23:36 +0530

    falsy method added

--- functions.js
+++ functions.js
@@ -1,8 +1,10 @@
 const functions = {
-    add: (num1, num2) => num1 + num2
+    add: (num1, num2) => num1 + num2,
     // add: function(num1, num2) {
     //     return num1 + num2;
     // }
+    isNull: () => null,
+    checkValue: (x) => x
 }
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,15 @@ const functions = require('./functions');
 test('Adds 2 + 2 to equal 4', () => {
     expect(functions.add(2, 2)).toBe(4);
 });
+
+test('Adds 2 + 2 to NOT equal 5', () => {
+    expect(functions.add(2, 2)).not.toBe(5);
+});
+
+test('Should be null', () => {
+    expect(functions.isNull()).toBeNull();
+});
+
+test('Should be falsy', () => {
+    expect(functions.checkValue(null)).toBeFalsy();
+});
